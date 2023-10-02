788a1cfb0d170da87165d18ac1b818494e0920f9
Chrstn67 <chrstn.hmbrt67@outlook.com>
2023-10-02 18:40:04 +0200

    erreur lien

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main>
-      <Link href="/accueil"></Link>
+      <Link href="/Accueil"></Link>
     </main>
   );
 }
